ea7a5a452a99f916d7d900043a4a7a46aab3540c
Danilo Cava <danilo.cavallini.01@gmail.com>
2023-05-22 16:12:51 +0200

    es2 - time: 1h:57

--- ESAMI/14Lug2022_2/src/it/unibo/paw/hibernate/util/HibernateUtil.java
+++ ESAMI/14Lug2022_2/src/it/unibo/paw/hibernate/util/HibernateUtil.java
@@ -8,44 +8,15 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 	private static SessionFactory sessionFactory = initHibernateUtil();
-
-	private static final String CREATE_OSPEDALI = "CREATE TABLE ospedali(" +
-			"	ospId INT NOT NULL PRIMARY KEY," +
-			"	codiceOsp int NOT NULL UNIQUE," +
-			"	nome varchar(50)," +
-			"	citta varchar(50)," +
-			"	indirizzo varchar(50)" +
-			")";
-	private static final String CREATE_TIPOACCERTAMENTO = "CREATE TABLE tipoAccertamento(" +
-			"	tipoAccId INT NOT NULL PRIMARY KEY," +
-			"	codiceTipoAcc int NOT NULL UNIQUE," +
-			"	descrizione varchar(255)" +
-			")";
-
-	private static final String CREATE_TIPOACCERTAMENTO_OSPEDALE = "CREATE TABLE tipoAccertamento_ospedale(" +
-			"	ospId INT NOT NULL," +
-			"	tipoAccId INT NOT NULL," +
-			"	PRIMARY KEY(ospId, tipoAccId)," +
-			"	FOREIGN KEY (ospId) REFERENCES ospedali(ospId)," +
-			"	FOREIGN KEY (tipoAccId) REFERENCES tipoAccertamento(tipoAccId)" +
-			")";
-	private static final String CREATE_ACCERTAMENTI = "CREATE TABLE accertamenti(" +
-			"	accID INT NOT NULL PRIMARY KEY," +
-			"	codiceAcc int NOT NULL UNIQUE," +
-			"	nome varchar(50)," +
-			"	descrizione varchar(255)," +
-			"	tipoAccId int," +
-			"	FOREIGN KEY (tipoAccId) REFERENCES tipoAccertamento(tipoAccId)" +
-			")";
 	
 	private static final String CREATE_OGGETTI = "CREATE TABLE oggetto_digitale(" +
 			"id INT NOT NULL PRIMARY KEY," + 
 			"codiceOggetto INT NOT NULL UNIQUE," + 
 			"nome varchar(50)," +
 			"formato varchar(50)," +
-			"dataDigitalizzaione DATE," +
-			"materiale_id INT NOT NULL" +
-			"FOREIGN KEY (materiale_id) REFERENCES materiale_fisico(id)";
+			"dataDigitalizzazione DATE," +
+			"materiale_id INT," +
+			"FOREIGN KEY (materiale_id) REFERENCES materiale_fisico(id))";
 	
 	private static final String CREATE_MATERIALE = "CREATE TABLE materiale_fisico(" +
 			"id INT NOT NULL PRIMARY KEY," + 
@@ -53,15 +24,15 @@ public class HibernateUtil {
 			"nome varchar(50)," +
 			"descrizione varchar(50)," +
 			"dataCreazione DATE," +
-			"archivio_id INT NOT NULL" +
-			"FOREIGN KEY (archivio_id) REFERENCES archivio_fisico(id)";
+			"archivio_id INT," +
+			"FOREIGN KEY (archivio_id) REFERENCES archivio_fisico(id))";
 	
 	private static final String CREATE_ARCHIVIO = "CREATE TABLE archivio_fisico(" +
 			"id INT NOT NULL PRIMARY KEY," + 
 			"codiceArchivio INT NOT NULL UNIQUE," + 
 			"nome varchar(50)," +
 			"descrizione varchar(50)," +
-			"dataCreazione DATE";
+			"dataCreazione DATE)";
 
 	private static SessionFactory initHibernateUtil() {
 		try {
--- ESAMI/14Lug2022_2/test/it/unibo/paw/hibernate/HibernateTest.java
+++ ESAMI/14Lug2022_2/test/it/unibo/paw/hibernate/HibernateTest.java
@@ -1,8 +1,9 @@
 package it.unibo.paw.hibernate;
 
 import java.io.FileWriter;
-
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.OffsetDateTime;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
@@ -28,6 +29,29 @@ public class HibernateTest {
 	 * "join ta.accertamenti a " + "group by o.nome, o.indirizzo, o.citta";
 	 */
 
+	/*
+	 * private static final String FIRST_QUERY =
+	 * "SELECT a.NOME, count(DISTINCT o.FORMATO)  FROM ARCHIVIO_FISICO a " +
+	 * "JOIN MATERIALE_FISICO m ON m.archivio_id = a.id " +
+	 * "JOIN OGGETTO_DIGITALE o ON o.materiale_id = m.id " +
+	 * "WHERE YEAR(a.dataCreazione) > 2014 GROUP BY a.NOME ";
+	 */
+
+	/*
+	 * private static final String FIRST_QUERY =
+	 * "select count(distinct o.formato) from " +
+	 * OggettoDigitale.class.getSimpleName() +
+	 * " o join o.materiale_id m join m.archivio_id a where year(a.dataCreazione) > 2014"
+	 * ;
+	 */
+
+	private static final String FIRST_QUERY = "select a.nome, count(distinct o.formato) from "
+			+ ArchivioFisico.class.getSimpleName()
+			+ " a join a.materiali m join m.oggetti o where year(a.dataCreazione) > 2014 group by a.nome";
+	
+	private static final String SECOND_QUERY = "select distinct a.nome from " + ArchivioFisico.class.getSimpleName()
+			+ " a join a.materiali m join m.oggetti o where o.formato = 'jpeg'";
+
 	public static void main(String[] argv) {
 
 		Session session = null;
@@ -47,112 +71,108 @@ public class HibernateTest {
 			a.setCodiceArchivio(1);
 			a.setNome("archvio1");
 			a.setDescrizione("archvio1");
-			calendar.set(1, 1, 2022);
+			calendar.set(2022, 1, 1);
 			a.setDataCreazione(calendar.getTime());
 
-			session.persist(a);
+			ArchivioFisico a2 = new ArchivioFisico();
+			a2.setId(2);
+			a2.setCodiceArchivio(2);
+			a2.setNome("archvio2");
+			a2.setDescrizione("archvio2");
+			calendar.set(2022, 1, 1);
+			a2.setDataCreazione(calendar.getTime());
 
 			MaterialeFisico m = new MaterialeFisico();
 			m.setId(1);
 			m.setCodiceMateriale(1);
 			m.setNome("materiale1");
 			m.setDescrizione("materiale1");
-			calendar.set(1, 1, 2022);
+			calendar.set(2022, 1, 1);
 			m.setDataCreazione(calendar.getTime());
 
-			session.persist(m);
+			MaterialeFisico m2 = new MaterialeFisico();
+			m2.setId(2);
+			m2.setCodiceMateriale(2);
+			m2.setNome("materiale1");
+			m2.setDescrizione("materiale1");
+			calendar.set(2022, 1, 1);
+			m2.setDataCreazione(calendar.getTime());
 
 			OggettoDigitale o = new OggettoDigitale();
 			o.setId(1);
 			o.setCodiceOggetto(1);
 			o.setNome("materiale1");
 			o.setFormato("jpeg");
-			calendar.set(1, 1, 2022);
+			calendar.set(2022, 1, 1);
 			o.setDataDigitalizzazione(calendar.getTime());
-			
+
 			OggettoDigitale o2 = new OggettoDigitale();
-			o.setId(2);
-			o.setCodiceOggetto(2);
-			o.setNome("materiale2");
-			o.setFormato("png");
-			calendar.set(1, 3, 2020);
-			o.setDataDigitalizzazione(calendar.getTime());
+			o2.setId(2);
+			o2.setCodiceOggetto(2);
+			o2.setNome("materiale2");
+			o2.setFormato("jpeg");
+			calendar.set(2020, 3, 1);
+			o2.setDataDigitalizzazione(calendar.getTime());
+
+			session.persist(o);
+			session.persist(o2);
 
 			Set<OggettoDigitale> oggetti = new HashSet<OggettoDigitale>();
 			oggetti.add(o2);
 			oggetti.add(o);
+			Set<OggettoDigitale> oggetti2 = new HashSet<OggettoDigitale>();
+			oggetti2.add(o);
 			m.setOggetti(oggetti);
-			
+			m2.setOggetti(oggetti2);
+
 			Set<MaterialeFisico> materiali = new HashSet<MaterialeFisico>();
 			materiali.add(m);
+
+			Set<MaterialeFisico> materiali2 = new HashSet<MaterialeFisico>();
+			materiali2.add(m2);
+
 			a.setMateriali(materiali);
-			
+			a2.setMateriali(materiali2);
+
 			session.persist(m);
+			session.persist(m2);
 			session.persist(a);
+			session.persist(a2);
 
 			tx.commit();
 			session.close();
 
-			/*session = HibernateUtil.getSessionFactory().openSession();
+			session = HibernateUtil.getSessionFactory().openSession();
 
-			String firstQueryResult = "";
-			String secondQueryResult = "";
+			Query firstQuery = session.createQuery(FIRST_QUERY);
 
-			// Approccio DB-Oriented
-			Query firstQuery = session.createQuery(FIRSTQUERY_STR);
-			firstQuery.setString("cittaOsp", "Bologna");
-			firstQuery.setString("nomeOsp", "S.Orsola");
-			firstQuery.setString("descTipoAcc", "analisi di laboratorio");
-			List<String> firstQueryRecords = firstQuery.list();
-			for (String nome : firstQueryRecords) {
-				firstQueryResult += nome + "\n";
+			PrintWriter pw = new PrintWriter(new FileWriter("Archivio.txt"));
+			
+			pw.println("PRIMA QUERY");
+			List<Object[]> res = firstQuery.list();
+			for (Object[] result : res) {
+				System.out.println(result[0] + "-" + result[1]);
+				pw.println(result[0] + "-" + result[1]);
 			}
+			
+			Query secondQuery = session.createQuery(SECOND_QUERY);
 
-			Query secondQuery = session.createQuery(SECONDQUERY_STR);
-			List<Object[]> secondQueryRecords = secondQuery.list();
-			for (Object[] arr : secondQueryRecords) {
-				secondQueryResult += arr[0] + " " + arr[1] + " " + arr[2] + "\n Numero Accertamenti erogabili: "
-						+ arr[3] + "\n";
-			}*/
-
-			/*
-			 * //Approccio Java-Oriented Query firstQuery = session.createQuery("from " +
-			 * Ospedale.class.getSimpleName() + " where citta = ? and nome = ?");
-			 * firstQuery.setString(0, "Bologna"); firstQuery.setString(1, "S.Orsola");
-			 * List<Ospedale> ospedaliRes = firstQuery.list();
-			 * 
-			 * for (Ospedale os : ospedaliRes) { Set<TipoAccertamento> tipiAcc =
-			 * os.getTipiAccertamento(); for (TipoAccertamento tipo : tipiAcc) { if
-			 * (tipo.getDescrizione().compareTo("analisi di laboratorio") == 0) {
-			 * Set<Accertamento> accertamenti = tipo.getAccertamenti(); for (Accertamento
-			 * acc : accertamenti) { firstQueryResult = firstQueryResult + acc.getNome() +
-			 * "\n"; } } } }
-			 * 
-			 * Query secondQuery = session.createQuery("from " +
-			 * Ospedale.class.getSimpleName()); ospedaliRes = secondQuery.list(); for
-			 * (Ospedale os : ospedaliRes) { secondQueryResult = secondQueryResult +
-			 * os.getNome() + " " + os.getIndirizzo() + " " + os.getCitta() +
-			 * "\n Numero Accertamenti erogabili: "; Set<TipoAccertamento> tipiAcc =
-			 * os.getTipiAccertamento(); int numAccCounter = 0; for (TipoAccertamento tipo :
-			 * tipiAcc) { numAccCounter += tipo.getAccertamenti().size(); }
-			 * secondQueryResult = secondQueryResult + numAccCounter + "\n"; }
-			 */
-
-			/*PrintWriter pw = new PrintWriter(new FileWriter("Ospedali.txt"));
-			pw.println(firstQueryResult);
-			pw.append(secondQueryResult);
+			pw.println("\nSECONDA QUERY");
+			List<String> res2 = secondQuery.list();
+			for (String result : res2) {
+				System.out.println(result);
+				pw.println(result);
+			}
+			
 			pw.close();
-		} catch (Exception e1) {
-			if (tx != null) {
-				try {
-					tx.rollback();
-				} catch (Exception e2) {
-					e2.printStackTrace();
-				}
-			}
-			e1.printStackTrace();*/
+		} catch (IOException e) {
+			e.printStackTrace();
 		} finally {
-			if (session != null) {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+
+			if (session.isOpen()) {
 				session.close();
 			}
 		}
